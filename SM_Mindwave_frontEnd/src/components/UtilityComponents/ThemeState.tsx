3e2fd0f5f30b739b83f72755af73d2a39f7c949c
Rolandos Georgoulis <58527036+RolandosG@users.noreply.github.com>
2024-01-10 12:53:13 -0500

    Delete SM_Mindwave_frontEnd directory

--- SM_Mindwave_frontEnd/src/components/UtilityComponents/ThemeState.tsx
+++
@@ -1,7 +0,0 @@
-// themeState.js
-import { atom } from 'recoil';
-
-export const themeState = atom({
-  key: 'themeState', // unique ID (with respect to other atoms/selectors)
-  default: 'light', // default value (aka initial value)
-});
