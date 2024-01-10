8595c606ec4904af75499645395f637231db65b8
Rolandos Georgoulis <58527036+RolandosG@users.noreply.github.com>
2024-01-10 12:53:04 -0500

    Delete SM_Mindwave_backend directory

--- SM_Mindwave_backend/lambda.ts
+++
@@ -1,9 +0,0 @@
-import awsServerlessExpress from 'aws-serverless-express';
-import app from './app';
-
-const server = awsServerlessExpress.createServer(app);
-
-exports.handler = (event, context) => {
-  // Pass the AWS event and context to awsServerlessExpress
-  awsServerlessExpress.proxy(server, event, context);
-};
