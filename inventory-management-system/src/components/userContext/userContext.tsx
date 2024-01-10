3ef7c7d1efdb6331400c793e3df17eb1d370779f
Rolandos Georgoulis <58527036+RolandosG@users.noreply.github.com>
2024-01-10 12:53:26 -0500

    Delete inventory-management-system directory

--- inventory-management-system/src/components/userContext/userContext.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const UserContext = React.createContext({
-    loggedInUserId: null, // default value
-    // ... you can add more user-related data here
-  });
-  
-  export default UserContext;
